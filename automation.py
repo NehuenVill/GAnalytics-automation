8c4c5442e01b8a36e59646dae43f881dbb55a403
JohnSolutions <jonhsmithd930@gmail.com>
2023-01-18 17:37:46 -0300

    Created get_metrics_data() function
    Finished the first version of the function, it now retrieves information from all the available metrics and store them in the data variable as a dict.

--- automation.py
+++ automation.py
@@ -18,7 +18,108 @@ metrics_list = ['activeUsers',
                 'advertiserAdCost',
                 'advertiserAdCostPerClick',
                 'advertiserAdCostPerConversion',
-                '']
+                'advertiserAdImpressions',
+                'averagePurchaseRevenue',
+                'averagePurchaseRevenuePerPayingUser',
+                'averagePurchaseRevenuePerUser',
+                'averageRevenuePerUser',
+                'averageSessionDuration',
+                'bounceRate',
+                'cartToViewRate',
+                'checkouts',
+                'cohortActiveUsers',
+                'cohortTotalUsers',
+                'conversions',
+                'crashAffectedUsers',
+                'crashFreeUsersRate',
+                'dauPerMau',
+                'dauPerWau',
+                'ecommercePurchases',
+                'engagedSessions',
+                'engagementRate',
+                'eventCount',
+                'eventCountPerUser',
+                'eventValue',
+                'eventsPerSession',
+                'firstTimePurchaserConversionRate',
+                'firstTimePurchasers',
+                'firstTimePurchasersPerNewUser',
+                'itemListClickEvents',
+                'itemListClickThroughRate',
+                'itemListViewEvents',
+                'itemPromotionClickThroughRate',
+                'itemRevenue',
+                'itemViewEvents',
+                'itemsAddedToCart',
+                'itemsCheckedOut',
+                'itemsClickedInList',
+                'itemsClickedInPromotion',
+                'itemsPurchased',
+                'itemsViewed',
+                'itemsViewedInList',
+                'itemsViewedInPromotion',
+                'newUsers',
+                'organicGoogleSearchAveragePosition',
+                'organicGoogleSearchClickThroughRate',
+                'organicGoogleSearchClicks',
+                'organicGoogleSearchImpressions',
+                'promotionClicks',
+                'promotionViews',
+                'publisherAdClicks',
+                'publisherAdImpressions',
+                'purchaseRevenue',
+                'purchaseToViewRate',
+                'purchaserConversionRate',
+                'returnOnAdSpend',
+                'screenPageViews',
+                'screenPageViewsPerSession',
+                'sessionConversionRate',
+                'sessions',
+                'sessionsPerUser',
+                'shippingAmount',
+                'taxAmount',
+                'totalAdRevenue',
+                'totalPurchasers',
+                'totalRevenue',
+                'totalUsers',
+                'transactions',
+                'transactionsPerPurchaser',
+                'userConversionRate',
+                'userEngagementDuration',
+                'wauPerMau'
+                ]
+
+def get_metrics_data(ml, p_id, start_date, end_date) -> dict:
+
+    """Function to get data from the GA API.
+        
+        ml = metrics list."""
+
+    data = {}
+
+    for metric in ml:
+
+        request = RunReportRequest(
+        property=f"properties/{p_id}",
+        metrics=[Metric(name=metric)],
+        date_ranges=[DateRange(start_date=start_date, end_date=end_date)]
+        )
+
+        full_response = client.run_report(request)
+
+        data[metric] = full_response['rows']['metric_values']['value']
+
+
+    
+
+
+def save_to_excel(metrics_data):
+
+    pass
+
+def send_mail(excel_file):
+
+    pass
 
 def test(request):
 
