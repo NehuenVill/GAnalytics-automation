ea8a000d23163e82c878952de60348ad20977966
JohnSolutions <jonhsmithd930@gmail.com>
2023-03-03 20:37:09 -0300

    Added new metrics

--- automation.py
+++ automation.py
@@ -1,9 +1,9 @@
 from os import environ, remove
 import ssl
-from tkinter.tix import Tree
+from time import strftime
 import pandas as pd
 import smtplib
-from datetime import date, datetime, timedelta
+from datetime import date, timedelta
 from dateutil.relativedelta import relativedelta
 from os.path import basename
 from email.mime.application import MIMEApplication
@@ -12,12 +12,16 @@ from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import DateRange
-from google.analytics.data_v1beta.types import Dimension
 from google.analytics.data_v1beta.types import Metric
+from google.analytics.data_v1beta.types import Dimension
 from google.analytics.data_v1beta.types import RunReportRequest
-from google.analytics.data_v1beta.types import OrderBy
+from google.analytics.data_v1beta.types import CohortSpec, CohortReportSettings
 from google.api_core.exceptions import InvalidArgument
-
+import openpyxl
+import sqlite3
+from sqlite3 import IntegrityError
+from time import sleep
+ 
 property_id = '347990037'
 environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'Proyecto API-8b9749be5c38.json'
 
@@ -45,6 +49,29 @@ metrics_list = ['Date',
                 'wauPerMau'
                 ]
 
+
+def insert_to_db(f_data):
+
+    print(f'Inserting data: {f_data} into the database')
+    
+    connection = sqlite3.connect("Prensa_base_de_datos.db")
+    cursor = connection.cursor()
+
+    try:
+
+        cursor.execute(f"""INSERT INTO Analytics_data VALUES ('{str(f_data['Date'])}',{f_data['activeUsers']},{f_data['averageSessionDuration']},{f_data['bounceRate']},
+        {f_data['crashFreeUsersRate']},{f_data['dauPerMau']},{f_data['dauPerWau']},{f_data['engagedSessions']},
+        {f_data['engagementRate']}, {f_data['eventCount']}, {f_data['eventCountPerUser']}, {f_data['eventsPerSession']}, {f_data['newUsers']},
+        {f_data['screenPageViews']}, {f_data['screenPageViewsPerSession']}, {f_data['sessions']}, {f_data['sessionsPerUser']},
+        {f_data['totalUsers']}, {f_data['userEngagementDuration']}, {f_data['wauPerMau']})""")
+
+    except IntegrityError:
+
+        print('Already exists')
+
+    connection.commit()
+    connection.close()
+
 def set_dates(date_range):
 
     end_date = date.today()
@@ -71,7 +98,7 @@ def set_dates(date_range):
 
     return [end_date, start_date]
 
-def get_metrics_data(d_range, email, password) -> dict:
+def get_metrics_dataOLDVERSION(d_range, email, password) -> dict:
 
     """Function to get data from the GA API.
         
@@ -88,7 +115,7 @@ def get_metrics_data(d_range, email, password) -> dict:
 
     for i in range(7,0,-1):
 
-        data[i-1]['Date'] = (today - timedelta(days=i)).strftime('%m-%d-%Y')
+        data[i-1]['Date'] = (today - timedelta(days=i+1)).strftime('%m-%d-%Y')
 
         print(f"Getting info from date: {data[i-1]['Date']}")
 
@@ -107,7 +134,7 @@ def get_metrics_data(d_range, email, password) -> dict:
                 request = RunReportRequest(
                     property=f"properties/{property_id}",
                     metrics=[Metric(name=metric)],
-                    date_ranges=[DateRange(start_date=f'{i}daysAgo', end_date=f'{i-1}daysAgo')]
+                    date_ranges=[DateRange(start_date=f'{i+1}daysAgo', end_date=f'{i+1}daysAgo')]
                 )
 
                 full_response = client.run_report(request)
@@ -137,13 +164,279 @@ def get_metrics_data(d_range, email, password) -> dict:
 
     send_mail(email, password, start_date, end_date)
 
+def get_data_manual(start_date, end_date, email, password):
+
+    start = date(year=int(start_date.split('-')[0]), month=int(start_date.split('-')[1]),day=int(start_date.split('-')[2]))
+
+    end = date(year=int(end_date.split('-')[0]),month=int(end_date.split('-')[1]),day=int(end_date.split('-')[2]))
+
+    data = []
+
+    day_num = 0
+
+    while True:
+
+        data.append({'Date': start.strftime('%m/%d/%Y').replace('2023', '23').replace('2024', '24')})
+
+        print(f"Getting info from date: {data[day_num]['Date']}")
+
+        st_date_range = start.strftime('%Y-%m-%d')
+
+        for j in range(4,5):
+
+            print('-'*100)
+
+            print(f'\nGetting metrics')
+
+            try:
+
+                if j == 0:
+
+                    request = RunReportRequest(
+                        property=f"properties/{property_id}",
+                        metrics=[Metric(name=metrics_list[1]), Metric(name=metrics_list[2]), Metric(name=metrics_list[3]), Metric(name=metrics_list[4]), 
+                        Metric(name=metrics_list[5]), Metric(name=metrics_list[6]), Metric(name=metrics_list[7]), Metric(name=metrics_list[8]), Metric(name=metrics_list[9]), 
+                        Metric(name=metrics_list[10]) ],
+                        date_ranges=[DateRange(start_date=st_date_range, end_date=st_date_range)],
+                    )
+                
+
+                    full_response = client.run_report(request)
+
+                    try:
+
+                        for n, metric in enumerate(full_response.rows[0].metric_values):
+
+                            data[day_num][full_response.metric_headers[n].name] = metric.value
+
+                            print(f'Extracted metric: {full_response.metric_headers[n].name} ---- value = {data[day_num][full_response.metric_headers[n].name]}')
+
+                    except IndexError:
+
+                        print(f'metric: {metric} not available.')
+
+                        data[day_num][metric] = 'Not available'
+
+                elif j == 1:
+
+                    request = RunReportRequest(
+                        property=f"properties/{property_id}",
+                        metrics=[Metric(name=metrics_list[11]), Metric(name=metrics_list[12]), Metric(name=metrics_list[13]), Metric(name=metrics_list[14]), 
+                        Metric(name=metrics_list[15]), Metric(name=metrics_list[16]), Metric(name=metrics_list[17]), Metric(name=metrics_list[18]), Metric(name=metrics_list[19])],
+                        date_ranges=[DateRange(start_date=st_date_range, end_date=st_date_range)]
+                    )
+
+                    full_response = client.run_report(request)
+
+                    try:
+
+                        for n, metric in enumerate(full_response.rows[0].metric_values):
+
+                            data[day_num][full_response.metric_headers[n].name] = metric.value
+
+                            print(f'Extracted metric: {full_response.metric_headers[n].name} ---- value = {data[day_num][full_response.metric_headers[n].name]}')
+
+                    except IndexError:
+
+                        print(f'metric: {metric} not available.')
+
+                        data[day_num][metric] = 'Not available'
+
+                elif j == 2:
+
+                    request = RunReportRequest(
+                        property=f"properties/{property_id}",
+                        metrics=[Metric(name=metrics_list[17])],
+                        dimensions=[Dimension(name='newVsReturning')],
+                        date_ranges=[DateRange(start_date=st_date_range, end_date=st_date_range)]
+                    )
+
+                    full_response = client.run_report(request)
+
+                    try:
+
+                        new = full_response.rows[0].metric_values[0].value
+
+                        returning = full_response.rows[1].metric_values[0].value
+
+                        data[day_num]['New Users'] = new
+
+                        data[day_num]['Returning Users'] = returning
+
+                        print(f"Extracted metric: New Users ---- value = {data[day_num]['New Users']}")
+                        print(f"Extracted metric: Returning Users ---- value = {data[day_num]['Returning Users']}")
+
+                    except IndexError:
+
+                        print(f'metric: {metric} not available.')
+
+                        data[day_num][metric] = 'Not available'
+
+                elif j == 3:
+
+                    continue
+
+                    request = RunReportRequest(
+                        property=f"properties/{property_id}",
+                        metrics=[Metric(name=metrics_list[13])],
+                        dimensions=[Dimension(name='pagePath')],
+                        date_ranges=[DateRange(start_date=st_date_range, end_date=st_date_range)]
+                    )
+
+                    full_response = client.run_report(request)
+
+                    try:
+
+                        for n, metric in enumerate(full_response.rows[0].metric_values):
+
+                            data[day_num][full_response.metric_headers[n].name] = metric.value
+
+                            print(f'Extracted metric: {full_response.metric_headers[n].name} ---- value = {data[day_num][full_response.metric_headers[n].name]}')
+
+                    except IndexError:
+
+                        print(f'metric: {metric} not available.')
+
+                        data[day_num][metric] = 'Not available'
+                
+                elif j == 4:
+
+                    continue
+
+                    request = RunReportRequest(
+                        property=f"properties/{property_id}",
+                        metrics=[Metric(name=metrics_list[9])],
+                        dimensions=[Dimension(name='eventName')],
+                        date_ranges=[DateRange(start_date=st_date_range, end_date=st_date_range)]
+                    )
+
+                    full_response = client.run_report(request)
+
+                    try:
+
+                        for n, metric in enumerate(full_response.rows[0].metric_values):
+
+                            data[day_num][full_response.metric_headers[n].name] = metric.value
+
+                            print(f'Extracted metric: {full_response.metric_headers[n].name} ---- value = {data[day_num][full_response.metric_headers[n].name]}')
+
+                    except IndexError:
+
+                        print(f'metric: {metric} not available.')
+
+                        data[day_num][metric] = 'Not available'
+
+                print(full_response)
+
+                break
+
+            except InvalidArgument:
+
+                print('Could not get info from metrics metrics requested')
+
+        start = start + timedelta(days=1)
+
+        day_num += 1
+
+        if start > end:
+
+            break
+
+    save_to_excel(data, start_date, end_date)
+
+    send_mail(email, password, start_date, end_date)
+
+def get_metrics_data(d_range, email, password) -> dict:
+
+    """Function to get data from the GA API.
+        
+    """
+
+    date_range = set_dates(d_range)
+
+    end_date = date_range[0]
+    start_date = date_range[1]
+
+    today = date.today()
+
+    data = [{}, {}, {}, {}, {}, {}, {}]
+
+    for i in range(7,0,-1):
+
+        data[i-1]['Date'] = (today - timedelta(days=i+1)).strftime('%m/%d/%Y').replace('2023', '23').replace('2024', '24')
+
+        print(f"Getting info from date: {data[i-1]['Date']}")
+
+        for j in range(0,2):
+
+            print('-'*100)
+
+            print(f'\nGetting metrics')
+
+            try:
+
+                if j == 0:
+
+                    request = RunReportRequest(
+                        property=f"properties/{property_id}",
+                        metrics=[Metric(name=metrics_list[1]), Metric(name=metrics_list[2]), Metric(name=metrics_list[3]), Metric(name=metrics_list[4]), 
+                        Metric(name=metrics_list[5]), Metric(name=metrics_list[6]), Metric(name=metrics_list[7]), Metric(name=metrics_list[8]), Metric(name=metrics_list[9]), 
+                        Metric(name=metrics_list[10]) ],
+                        date_ranges=[DateRange(start_date=f'{i+1}daysAgo', end_date=f'{i+1}daysAgo')]
+                    )
+
+                    full_response = client.run_report(request)
+
+                elif j == 1:
+
+                    request = RunReportRequest(
+                        property=f"properties/{property_id}",
+                        metrics=[Metric(name=metrics_list[11]), Metric(name=metrics_list[12]), Metric(name=metrics_list[13]), Metric(name=metrics_list[14]), 
+                        Metric(name=metrics_list[15]), Metric(name=metrics_list[16]), Metric(name=metrics_list[17]), Metric(name=metrics_list[18]), Metric(name=metrics_list[19])],
+                        date_ranges=[DateRange(start_date=f'{i+1}daysAgo', end_date=f'{i+1}daysAgo')]
+                    )
+
+                    full_response = client.run_report(request)
+
+            except InvalidArgument:
+
+                print('Could not get info from metrics metrics requested')
+
+            try:
+
+                for n, metric in enumerate(full_response.rows[0].metric_values):
+
+                    data[i-1][full_response.metric_headers[n].name] = metric.value
+
+                    print(f'Extracted metric: {full_response.metric_headers[n].name} ---- value = {data[i-1][full_response.metric_headers[n].name]}')
+
+            except IndexError:
+
+                print(f'metric: {metric} not available.')
+
+                sleep(50)
+
+                data[i-1][metric] = 'Not available'
+
+    data.reverse()
+
+    save_to_excel(data, start_date, end_date)
+
+    for row in data:
+
+        insert_to_db(row)
+
+    send_mail(email, password, start_date, end_date)
+
+
 def save_to_excel(metrics_data, start_date, end_date):
 
     print('Saving to excel...')
 
-    df = pd.DataFrame(metrics_data, index=pd.RangeIndex(0,7), columns= metrics_list)
-
+    df = pd.DataFrame(metrics_data, index =pd.RangeIndex(0,len(metrics_data)) ,columns = metrics_list)
+    
     df.to_excel(f"Metricas_desde_{start_date}_a_{end_date}.xlsx", index=True, columns=metrics_list)
+
 
 def send_mail(email, password, start_date, end_date):
 
@@ -180,6 +473,49 @@ def send_mail(email, password, start_date, end_date):
 
     print('email sent successfully.')
 
+def export_hist_data_to_db():
+
+    workbook = openpyxl.load_workbook('Datos_históricos_Google_Analytics.xlsx', data_only=True)
+
+    worksheet = workbook['Hoja1']
+
+    for row in worksheet.iter_rows(min_row=2, max_row=1418):
+
+        data = {}
+
+        for i, metric in enumerate(metrics_list):
+
+            if row[i].value == 'Not available':
+
+                data[metric] = 'Null'
+
+            elif '-' in str(row[i].value):
+
+                data[metric] = str(row[i].value).replace('-', '/').replace(' 00:00:00', '').replace('2010','10').replace('2011','11').replace('2012','12')
+
+            elif ':' in str(row[i].value):
+
+                print(str(row[i].value))
+
+                minutes = int(str(row[i].value).split(':')[0])
+                seconds = int(str(row[i].value).split(':')[1])
+
+                final_time = float(minutes * 60 + seconds)
+
+                data[metric] = final_time
+
+            else:
+
+                data[metric] = row[i].value
+
+        print('-'*100)
+
+        print(f'Exporting record: {data}')
+
+        print('-'*100)
+
+        insert_to_db(data)
+
 if __name__ == '__main__':
 
-    get_metrics_data('semanal', 'nehuenv620@gmail.com', 'vzkfvhnhhiooiiuk')
+    pass
--- run.py
+++ run.py
@@ -1,8 +1,17 @@
-from automation import get_metrics_data
+from automation import get_metrics_data, get_data_manual
 
 tu_email = 'nehuenv620@gmail.com'
 tu_contraseña = 'vzkfvhnhhiooiiuk'
-rango_de_dias = 'semanal'  # Puede ser: 'semanal', 'mensual' o una cantidad de días, como 1, 2, 15, 20, etc.
+rango_de_dias = 'semanal'
+
 #ejecución del programa:
 
-get_metrics_data(rango_de_dias, tu_email, tu_contraseña)
+#get_metrics_data(rango_de_dias, tu_email, tu_contraseña)
+
+#Ejecución de la función manual:
+
+Día_incial = '2023-01-10' #AAAA-MM-DD
+
+Día_final = '2023-01-15'
+
+get_data_manual(Día_incial, Día_final, tu_email, tu_contraseña)
