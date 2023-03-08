0f3107141b3d59293b862df75a613ac41ab9b3b3
JohnSolutions <jonhsmithd930@gmail.com>
2023-03-08 15:36:28 -0300

    Finished the first version of the get_historic_data.py script

--- get_historic_data.py
+++ get_historic_data.py
@@ -8,12 +8,14 @@ MAIN_BTNS = {
     'Overview' : "//span[text()='Overview']",
     'Behavior' : "//span[text()='Behavior']",
     'Events' : "//span[text()='Events']",
-    'Evolok' : "//span[text()='EVOLOK']",
+    'Top events' : "//span[text()='Top Events']",
+    'Evolok' : "//span[text()='EVOLOK']"
 }
 
 METRICS = {
     'Active users' : "//span[text()='Active Users']",
-    'New vs Returning' : "//span[text()='New vs Returning']" 
+    'New vs Returning' : "//span[text()='New vs Returning']",
+    'New users' : "//div[text()='New Users']"
 }
 
 AUDIENCE_METRICS = {
@@ -47,7 +49,9 @@ EXPORT = {
 }
 
 OTHERS = {
-    'Login_entitlement' : "//span[text()='Login/Entitlement']"
+    'Login entitlement' : "//span[text()='Login/Entitlement']",
+    'Select metric' : "//div[text()='Select a metric']",
+    'Metric input' : "ID-searchBox"
 }
  
 
@@ -55,6 +59,8 @@ def start_driver():
 
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://analytics.google.com/analytics/web/?authuser=2#/a6897643w13263073p13942924/")
+
+    sleep(150)
 
     return driver
 
@@ -110,6 +116,8 @@ def get_audience_overview_metrics():
             gsheets = driver.find_element(By.CLASS_NAME, EXPORT['GS'])
 
             gsheets.click()
+
+            google_sheets_save(driver)
 
             st_year += 2
             end_year += 2
@@ -175,13 +183,14 @@ def get_active_users():
 
         gsheets.click()
 
+        google_sheets_save(driver)
+
         st_year += 2
         end_year += 2
 
         if end_year > 2023:
 
             break
-
 
 def get_event_metrics():
 
@@ -198,6 +207,88 @@ def get_event_metrics():
     overview = driver.find_elements(By.XPATH, MAIN_BTNS['Overview'])[1]
 
     overview.click()
+
+    ov_dropdown = driver.find_element(By.CLASS_NAME, DROPDOWN['Dropdown'])
+
+    ov_dropdown.click()
+
+    ov_items = driver.find_elements(By.CLASS_NAME, DROPDOWN['Items'])
+
+    i = 2
+
+    while True:
+
+        ov_items[i].click()
+
+        date_selector = driver.find_element(By.CLASS_NAME, DATE_RANGE['Date selector'])
+
+        date_selector.click()
+
+        start = driver.find_element(By.CLASS_NAME, DATE_RANGE['Input start'])
+
+        start.send_keys(f'Jan 4, 2009')
+
+        end = driver.find_element(By.CLASS_NAME, DATE_RANGE['Input end'])
+
+        end.send_keys(f'Jan 3, 2023')
+
+        apply = driver.find_element(By.CLASS_NAME, DATE_RANGE['Apply'])
+
+        apply.click()
+
+        export = driver.find_element(By.CLASS_NAME, EXPORT['Export'])
+
+        export.click()
+
+        gsheets = driver.find_element(By.CLASS_NAME, EXPORT['GS'])
+
+        gsheets.click()
+
+        google_sheets_save(driver)
+
+        driver.refresh()
+
+        ov_dropdown = driver.find_element(By.CLASS_NAME, DROPDOWN['Dropdown'])
+
+        ov_dropdown.click()
+
+        ov_items = driver.find_elements(By.CLASS_NAME, DROPDOWN['Items'])
+
+        i += 1
+
+        if i == 5:
+
+            break
+        else:
+            pass
+
+def get_audience_behavior_data():
+
+    driver = start_driver()
+
+    audience = driver.find_element(By.XPATH, MAIN_BTNS['Audience']) 
+
+    audience.click()
+
+    behavior = driver.find_element(By.XPATH, MAIN_BTNS['Behavior']) 
+
+    behavior.click()
+
+    newvsret = driver.find_element(By.XPATH, METRICS['New vs Returning'])
+
+    newvsret.click()
+
+    select = driver.find_element(By.XPATH, OTHERS['Select metric'])
+
+    select.click()
+
+    metric_input = driver.find_element(By.CLASS_NAME, OTHERS['Metric input'])
+
+    metric_input.send_keys('New users')
+
+    new_users = driver.find_element(By.XPATH, METRICS['New Users'])
+
+    new_users.click()
 
     start = driver.find_element(By.CLASS_NAME, DATE_RANGE['Input start'])
 
@@ -219,17 +310,70 @@ def get_event_metrics():
 
     gsheets.click()
 
-def get_audience_behavior_data():
+    google_sheets_save(driver)
 
-    """
+def get_evolok_metrics():
+
     driver = start_driver()
-
-    audience = driver.find_element(By.XPATH, MAIN_BTNS['Audience']) 
-
-    audience.click()
 
     behavior = driver.find_element(By.XPATH, MAIN_BTNS['Behavior']) 
 
     behavior.click()
 
-    """
+    events = driver.find_element(By.XPATH, MAIN_BTNS['Events'])
+
+    events.click()
+
+    top = driver.find_element(By.XPATH, MAIN_BTNS['Top Events'])
+
+    top.click()
+
+    log_en = driver.find_element(By.XPATH, OTHERS['Login entitlement'])
+
+    log_en.click()
+
+    date_selector = driver.find_element(By.CLASS_NAME, DATE_RANGE['Date selector'])
+
+    date_selector.click()
+
+    start = driver.find_element(By.CLASS_NAME, DATE_RANGE['Input start'])
+
+    start.send_keys(f'Jan 4, 2009')
+
+    end = driver.find_element(By.CLASS_NAME, DATE_RANGE['Input end'])
+
+    end.send_keys(f'Jan 3, 2023')
+
+    apply = driver.find_element(By.CLASS_NAME, DATE_RANGE['Apply'])
+
+    apply.click()
+
+    export = driver.find_element(By.CLASS_NAME, EXPORT['Export'])
+
+    export.click()
+
+    gsheets = driver.find_element(By.CLASS_NAME, EXPORT['GS'])
+
+    gsheets.click()
+
+    google_sheets_save(driver)
+
+
+def google_sheets_save(driver : webdriver.Chrome):
+
+    driver.switch_to.window(driver.window_handles[1])
+
+    save_btn = driver.find_element(By.ID, 'confirmActionButton')
+
+    save_btn.click()
+
+    sleep(7.5)
+
+    driver.close()
+
+    driver.switch_to.window(driver.window_handles[0])
+
+
+if __name__ == '__main__':
+
+    start_driver()
